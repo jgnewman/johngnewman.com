eb580cda9f5a914cdfb73ca91422c4ecf77fa984
John <rescuecreative@gmail.com>
2021-12-08 23:42:52 -0500

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,81 @@
+import type { Configuration } from "webpack"
+import path from "path"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import { getIfUtils, removeEmpty } from "webpack-config-utils"
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin"
+import ESLintPlugin from "eslint-webpack-plugin"
+import { CleanWebpackPlugin } from "clean-webpack-plugin"
+import CopyPlugin from "copy-webpack-plugin"
+
+const { ifProduction } = getIfUtils(process.env.NODE_ENV ?? "development")
+
+const config: Configuration = {
+  mode: ifProduction("production", "development"),
+  output: ifProduction(
+    {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      publicPath: "",
+    },
+    {
+      publicPath: "/",
+    },
+  ),
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|ico)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: removeEmpty([
+    ifProduction(new CleanWebpackPlugin()),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false
+    }),
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/static-assets",
+          to: "assets",
+        },
+      ],
+    }),
+  ]),
+  devtool: "source-map",
+  devServer: {
+    static: path.join(__dirname, "build"),
+    historyApiFallback: true,
+  },
+}
+
+export default config
