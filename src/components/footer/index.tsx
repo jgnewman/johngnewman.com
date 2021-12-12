a9282c2f6e20ee853ffef66aeba8546ab41ae07e
John <rescuecreative@gmail.com>
2021-12-11 22:50:54 -0500

    create dynamic footer links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import cx from 'classnames'
 
-const Footer = () => {
+import navArrowUrl from '@/images/nav-arrow.svg'
+import useDynamicLinks from '@/hooks/useDynamicLinks'
+
+import {
+  footerWrapper,
+  copyright,
+  linkWrapper,
+  prevLinkWrapper,
+  nextLinkWrapper,
+  navLink,
+  prevLink,
+  nextLink,
+} from './styles.scss'
+
+const Footer = memo(() => {
+  const { prev, next } = useDynamicLinks()
+  const { pathname } = useLocation()
+
   return (
-    <></>
+    <footer className={footerWrapper}>
+
+      <section className={cx(linkWrapper, prevLinkWrapper)}>
+        {prev && (
+          <Link className={cx(navLink, prevLink)} to={prev.route}>
+            <img alt="arrow" src={navArrowUrl} />
+            <span>{prev.title}</span>
+          </Link>
+        )}
+      </section>
+
+      <section className={copyright}>
+        Copyright © {new Date().getFullYear()} John Newman
+      </section>
+
+      <section className={cx(linkWrapper, nextLinkWrapper)}>
+        {next && pathname !== '/' && (
+          <Link className={cx(navLink, nextLink)} to={next.route}>
+            <span>{next.title}</span>
+            <img alt="arrow" src={navArrowUrl} />
+          </Link>
+        )}
+      </section>
+
+    </footer>
   )
-}
+})
 
 export default Footer
--- src/components/page-grid/index.tsx
+++ src/components/page-grid/index.tsx
@@ -1,8 +1,6 @@
 import type { PageGridProps } from './types'
 import React, { memo } from 'react'
 
-import useDynamicLinks from '@/hooks/useDynamicLinks'
-
 import useBuildMenuFromGrid from './useBuildMenuFromGrid'
 import useGridChildren from './useGridChildren'
 import useGridDimensions from './useGridDimensions'
@@ -20,10 +18,6 @@ const PageGrid = memo(({ grid }: PageGridProps) => {
   useBuildMenuFromGrid(grid)
   useRedirect(shouldRedirect)
 
-  const { next, prev } = useDynamicLinks()
-  console.log({ next, prev })
-
-  // TODO: Handle header and footer with dynamic links
   return (
     <div className={pageGridWrapper}>
       <div className={pageGrid} style={{
